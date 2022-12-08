d6c59c34ac541420fa1e4ae602752e14d4bf2042
m-lemaire <60786784+m-lemaire@users.noreply.github.com>
2022-12-08 21:16:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+# import of the modules
+from flask import Flask, render_template
+import requests
+
 
 app = Flask(__name__)
 
+
+# print Hello World on the home page
 @app.route('/', methods=["GET"])
 def hello_world():
-
     prefix_google = """
     <!-- Google tag (gtag.js) -->
     <script async
@@ -17,3 +21,31 @@ def hello_world():
     </script>
     """
     return prefix_google + "Hello World"
+
+
+# add a route to print some logs
+@app.route('/logger', methods=["GET"])
+def logger():
+    script = """
+    <script> console.log("This is a log in the console")</script>"""
+    return "This is a log :)" + script
+
+
+# add a route to print the user input in the console from the textbox
+@app.route('/textbox', methods=["GET"])
+def print():
+    script = """
+    <script>
+        function myFunction() {
+        var message = document.getElementById("message").value;
+        console.log(message);
+        }
+    </script>"""
+    return """
+    <input type="text" id="message" placeholder="Enter a message">
+    <button onclick="myFunction()">Print</button>""" + script
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
