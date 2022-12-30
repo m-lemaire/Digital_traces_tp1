6c2d0b1289fa4d0de4712b3506fd564f3e43ab5f
Manon <manon.lemaire@epfedu.fr>
2022-12-30 09:58:18 +0100

    tp2 commit

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+# import of the modules
+from flask import Flask, render_template
+import requests
+
 
 app = Flask(__name__)
 
+
+# print Hello World on the home page
 @app.route('/', methods=["GET"])
 def hello_world():
-
     prefix_google = """
     <!-- Google tag (gtag.js) -->
     <script async
@@ -17,3 +21,38 @@ def hello_world():
     </script>
     """
     return prefix_google + "Hello World"
+
+
+# add a route to print some logs
+@app.route('/logger', methods=["GET"])
+def logger():
+    script = """
+    <script> console.log("This is a log in the console")</script>"""
+    return "This is a log :)" + script
+
+
+# add a route to print the user input in the console from the textbox
+@app.route('/textbox', methods=["GET"])
+def print():
+    script = """
+    <script>
+        function myFunction() {
+        var message = document.getElementById("message").value;
+        console.log(message);
+        }
+    </script>"""
+    return """
+    <input type="text" id="message" placeholder="Enter a message">
+    <button onclick="myFunction()">Print</button>""" + script
+
+@app.route('/cookies/', methods = ["GET"])
+def req():
+    #req = requests.get("https://www.google.com/")
+    req = requests.get("https://analytics.google.com/analytics/web/#/report-home/a251003635w345024245p281152693")
+    
+    # return req.cookies.get_dict()
+    return req.text
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
