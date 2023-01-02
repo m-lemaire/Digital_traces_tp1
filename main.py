4fc80fc4fce7347cff731c36a3b8b544c69d30dc
Manon <manon.lemaire@epfedu.fr>
2023-01-02 15:33:11 +0100

    number of visitors

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # import of the modules
 from flask import Flask, render_template
+from apiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
 import requests
+import lxml
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -52,6 +56,61 @@ def req():
     
     # return req.cookies.get_dict()
     return req.text
+
+
+
+
+SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
+KEY_FILE_LOCATION = 'digital-traces-373513-1489f264a109.json'
+VIEW_ID = '281152693' #You can find this in Google Analytics > Admin > Property > View > View Settings (VIEW ID)
+
+
+def initialize_analyticsreporting():
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+      KEY_FILE_LOCATION, SCOPES)
+    analytics = build('analyticsreporting', 'v4', credentials=credentials)
+
+    return analytics
+
+
+def get_report(analytics):
+  return analytics.reports().batchGet(
+      body={
+        'reportRequests': [
+        {
+          'viewId': VIEW_ID,
+          'dateRanges': [{'startDate': '30daysAgo', 'endDate': 'today'}],
+          'metrics': [{'expression': 'ga:pageviews'}],
+          'dimensions': []
+        }]
+      }
+  ).execute()
+
+
+def get_visitors(response):
+    visitors = 0 # in case there are no analytics available yet
+    for report in response.get('reports', []):
+        columnHeader = report.get('columnHeader', {})
+        metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+
+        for row in report.get('data', {}).get('rows', []):
+            dateRangeValues = row.get('metrics', [])
+
+            for i, values in enumerate(dateRangeValues):
+                for metricHeader, value in zip(metricHeaders, values.get('values')):
+                    visitors = value
+
+    return str(visitors)
+
+
+@app.route('/visitors')
+def visitors():
+    analytics = initialize_analyticsreporting()
+    response = get_report(analytics)
+    visitors = get_visitors(response)
+
+    # return render_template('visitors.html', visitors=str(visitors))
+    return "nombre de visiteurs : "+visitors
 
 if __name__ == '__main__':
 
